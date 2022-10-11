2a245e59322b40848c4f8dce6638fbb27c135f0c
Adrian Stępin <stepin.adrian@gmail.com>
2022-10-11 17:38:40 +0200

    add few new files

---
+++ basicSyntax.js
@@ -0,0 +1,9 @@
+document.write(" hi we are learning javascript, hello I am instructor")
+
+// DATATYPES//
+var A = 10; //integer
+var E = 10.5; //float
+var B = "hello"; //string
+var C = true;
+var D = false; //boolean   
+
